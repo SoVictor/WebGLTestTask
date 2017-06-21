24c87951ef71c1cfd638091c2c35267c6fbce26f
SoVictor <SoVictor_tomsk@hotmail.com>
2017-06-21 22:15:47 +0700

    White canvas instead of grey

--- app.js
+++ app.js
@@ -113,7 +113,7 @@ function renderFrame()
 {
 	gl.enable(gl.DEPTH_TEST);
 	
-	gl.clearColor(0.5, 0.5, 0.5, 0.9);
+	gl.clearColor(1.0, 1.0, 1.0, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT);
 	
 	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
