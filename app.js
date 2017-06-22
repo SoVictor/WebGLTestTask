7f92ecfab983c679ca6d576fd7ad5cf12d36a042
SoVictor <SoVictor_tomsk@hotmail.com>
2017-06-22 22:36:28 +0700

    changed functions order in app.js

--- app.js
+++ app.js
@@ -55,27 +55,242 @@ addHandler(window, 'mousewheel', processMouseWheel);
 addHandler(document, 'mousewheel', processMouseWheel);
 
 
-function worldToViewCoordinates( worldCoordinates )
+function init()
 {
-	let scale = scales[scaleIdx];
-	return {x: worldCoordinates.x * scale - offset.x, y: worldCoordinates.y * scale - offset.y};
-};
+	gl = getGLContext();
+	
+	pointShaderProgram = createPointShaderProgram();
+	
+	pointVertexBuffer = gl.createBuffer();
+
+	pointCoordLocation = gl.getAttribLocation(pointShaderProgram, "a_position");
+	pointResolutionUniformLocation = gl.getUniformLocation(pointShaderProgram, "u_resolution");
+	pointOffsetUniformLocation = gl.getUniformLocation(pointShaderProgram, "u_offset");
+	pointScaleUniformLocation = gl.getUniformLocation(pointShaderProgram, "u_scale");
+	
+
+	imageShaderProgram = createImageShaderProgram();
+	
+	imageVertexBuffer = gl.createBuffer();
+	imageTexCoordBuffer = gl.createBuffer();
+
+	imageCoordLocation = gl.getAttribLocation(imageShaderProgram, "a_position");
+	imageTexCoordLocation = gl.getAttribLocation(imageShaderProgram, "a_texCoord");
+	imageResolutionUniformLocation = gl.getUniformLocation(imageShaderProgram, "u_resolution");
+	imageOffsetUniformLocation = gl.getUniformLocation(imageShaderProgram, "u_offset");
+	imageScaleUniformLocation = gl.getUniformLocation(imageShaderProgram, "u_scale");
+	
+	
+	var image = new Image();
+	image.onload = function() {
+		gl.bindBuffer(gl.ARRAY_BUFFER, imageVertexBuffer);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([ 
+			0, 0,
+			image.width, 0,
+			0, image.height,
+			0, image.height,
+			image.width, 0,
+			image.width, image.height]), gl.STATIC_DRAW);
+			
+		gl.bindBuffer(gl.ARRAY_BUFFER, imageTexCoordBuffer);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+			0.0,  0.0,
+			1.0,  0.0,
+			0.0,  1.0,
+			0.0,  1.0,
+			1.0,  0.0,
+			1.0,  1.0]), gl.STATIC_DRAW);
+			
+		texture = gl.createTexture();
+		gl.bindTexture(gl.TEXTURE_2D, texture);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+	
+		renderFrame();
+	}
+	image.src = "Image.png";
+}
 
 
-function viewToWorldCoordinates( viewCoordinates )
-{
-	let scale = scales[scaleIdx];
-	return {x: (viewCoordinates.x + offset.x) / scale, y: (viewCoordinates.y + offset.y) / scale};
-};
-
-
-function getCursorPosition( mouseEvent )
+function getGLContext()
 {
 	var canvas = document.getElementById("canvas-element-id");
-	var canvasRect = canvas.getBoundingClientRect();
+	if (canvas == null)
+	{
+		alert("there is no canvas on this page");
+		return;
+	}
+	var names = ["webgl", "experimental-webgl", "webkit-3d", "moz-webgl"];
+	var ctx = null;
+	for (var i = 0; i < names.length; ++i) 
+	{
+		try 
+		{
+			ctx = canvas.getContext(names[i]);
+		} 
+		catch(e) {}
+		if (ctx) 
+			break;
+	}
+	if (ctx == null)
+	{
+		alert("WebGL is not available");
+	}
+	else
+	{
+		return ctx;
+	}
+}
 
-	return { x: (mouseEvent.clientX - canvasRect.left), y: (mouseEvent.clientY - canvasRect.top) };
-};
+
+function createPointShaderProgram()
+{
+	var vertCode =
+		'attribute vec3 a_position;' +
+		
+		'uniform vec2 u_resolution;' +
+		'uniform vec2 u_offset;' +
+		'uniform float u_scale;' +
+			
+		'void main(void) {' +
+			'vec2 clipSpace = ((vec2(a_position * u_scale) - u_offset) / u_resolution) * 2.0 - 1.0;' + 
+			'gl_Position = vec4(clipSpace * vec2(1, -1), 0.0, 1.0);' +
+			'gl_PointSize = a_position.z;'+
+		'}';
+	 var vertShader = gl.createShader(gl.VERTEX_SHADER);
+	 gl.shaderSource(vertShader, vertCode);
+	 gl.compileShader(vertShader);
+
+	 var fragCode =
+		'void main(void) {' +
+			' gl_FragColor = vec4(0.0, 0.5, 0.0, 1.0);' +
+		'}';
+	 var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+	 gl.shaderSource(fragShader, fragCode);
+	 gl.compileShader(fragShader);
+
+
+	 var shaderProgram = gl.createProgram();
+	 gl.attachShader(shaderProgram, vertShader); 
+	 gl.attachShader(shaderProgram, fragShader);
+	 gl.linkProgram(shaderProgram);
+	 
+	 return shaderProgram;
+}
+
+
+function createImageShaderProgram()
+{
+	var vertCode =
+		'attribute vec2 a_position;' +
+		'attribute vec2 a_texCoord;' +
+		
+		'uniform vec2 u_resolution;' +
+		'uniform vec2 u_offset;' +
+		'uniform float u_scale;' +
+		
+		'varying vec2 v_texCoord;' +
+			
+		'void main(void) {' +
+			'vec2 clipSpace = ((vec2(a_position * u_scale) - u_offset) / u_resolution) * 2.0 - 1.0;' + 
+			'gl_Position = vec4(clipSpace * vec2(1, -1), 0.0, 1.0);' +
+			'v_texCoord = a_texCoord;' +
+		'}';
+	 var vertShader = gl.createShader(gl.VERTEX_SHADER);
+	 gl.shaderSource(vertShader, vertCode);
+	 gl.compileShader(vertShader);
+
+	 var fragCode =
+		'precision mediump float;' +
+ 
+		'uniform sampler2D u_image;' +
+		 
+		'varying vec2 v_texCoord;' +
+		 
+		'void main() {' +
+		   'gl_FragColor = texture2D(u_image, v_texCoord);' +
+		'}';
+	 var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+	 gl.shaderSource(fragShader, fragCode);
+	 gl.compileShader(fragShader);
+
+
+	 var shaderProgram = gl.createProgram();
+	 gl.attachShader(shaderProgram, vertShader); 
+	 gl.attachShader(shaderProgram, fragShader);
+	 gl.linkProgram(shaderProgram);
+	 
+	 return shaderProgram;
+}
+
+
+function renderFrame()
+{
+	gl.enable(gl.DEPTH_TEST);
+	
+	gl.clearColor(1.0, 1.0, 1.0, 1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+	
+	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+	drawPoints();
+	drawImage();
+	
+	setTimeout(renderFrame, 20); // \todo: плохо, неименованная константа
+}
+
+
+function drawPoints()
+{
+	gl.useProgram(pointShaderProgram);
+	
+	gl.uniform2f(pointResolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+	gl.uniform2f(pointOffsetUniformLocation, offset.x, offset.y);
+	gl.uniform1f(pointScaleUniformLocation, scales[scaleIdx]);
+	
+	gl.bindBuffer(gl.ARRAY_BUFFER, pointVertexBuffer);
+	var vertices = []
+	points.forEach(function(point, idx) {
+		vertices.push(point.x);
+		vertices.push(point.y);
+		vertices.push( idx == selectedPointIdx ? 10 : 4 );
+	});
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+	gl.vertexAttribPointer(pointCoordLocation, 3, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(pointCoordLocation);
+
+	gl.drawArrays(gl.POINTS, 0, vertices.length / 3);
+}
+
+
+function drawImage()
+{
+	gl.useProgram(imageShaderProgram);
+	
+	gl.uniform2f(imageResolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+	gl.uniform2f(imageOffsetUniformLocation, offset.x, offset.y);
+	gl.uniform1f(imageScaleUniformLocation, scales[scaleIdx]);
+	
+	gl.bindBuffer(gl.ARRAY_BUFFER, imageVertexBuffer);
+	gl.vertexAttribPointer(imageCoordLocation, 2, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(imageCoordLocation);
+
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, imageTexCoordBuffer);
+	gl.vertexAttribPointer(imageTexCoordLocation, 2, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(imageTexCoordLocation);
+	
+	
+	gl.bindTexture(gl.TEXTURE_2D, texture);
+ 
+	// Set the parameters so we can render any size image.
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+	
+	gl.drawArrays(gl.TRIANGLES, 0, 6);
+}
 
 
 function processMouseDown( event ) 
@@ -185,245 +400,30 @@ function processMouseWheel( event )
 };
 
 
+function getCursorPosition( mouseEvent )
+{
+	var canvas = document.getElementById("canvas-element-id");
+	var canvasRect = canvas.getBoundingClientRect();
+
+	return { x: (mouseEvent.clientX - canvasRect.left), y: (mouseEvent.clientY - canvasRect.top) };
+};
+
+
+function worldToViewCoordinates( worldCoordinates )
+{
+	let scale = scales[scaleIdx];
+	return {x: worldCoordinates.x * scale - offset.x, y: worldCoordinates.y * scale - offset.y};
+};
+
+
+function viewToWorldCoordinates( viewCoordinates )
+{
+	let scale = scales[scaleIdx];
+	return {x: (viewCoordinates.x + offset.x) / scale, y: (viewCoordinates.y + offset.y) / scale};
+};
+
+
 function onSave()
 {
 	alert(JSON.stringify(points));
-}
-
-
-function renderFrame()
-{
-	gl.enable(gl.DEPTH_TEST);
-	
-	gl.clearColor(1.0, 1.0, 1.0, 1.0);
-	gl.clear(gl.COLOR_BUFFER_BIT);
-	
-	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-	drawPoints();
-	drawImage();
-	
-	setTimeout(renderFrame, 20); // \todo: плохо, неименованная константа
-}
-
-
-function drawPoints()
-{
-	gl.useProgram(pointShaderProgram);
-	
-	gl.uniform2f(pointResolutionUniformLocation, gl.canvas.width, gl.canvas.height);
-	gl.uniform2f(pointOffsetUniformLocation, offset.x, offset.y);
-	gl.uniform1f(pointScaleUniformLocation, scales[scaleIdx]);
-	
-	gl.bindBuffer(gl.ARRAY_BUFFER, pointVertexBuffer);
-	var vertices = []
-	points.forEach(function(point, idx) {
-		vertices.push(point.x);
-		vertices.push(point.y);
-		vertices.push( idx == selectedPointIdx ? 10 : 4 );
-	});
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
-	gl.vertexAttribPointer(pointCoordLocation, 3, gl.FLOAT, false, 0, 0);
-	gl.enableVertexAttribArray(pointCoordLocation);
-
-	gl.drawArrays(gl.POINTS, 0, vertices.length / 3);
-}
-
-
-function drawImage()
-{
-	gl.useProgram(imageShaderProgram);
-	
-	gl.uniform2f(imageResolutionUniformLocation, gl.canvas.width, gl.canvas.height);
-	gl.uniform2f(imageOffsetUniformLocation, offset.x, offset.y);
-	gl.uniform1f(imageScaleUniformLocation, scales[scaleIdx]);
-	
-	gl.bindBuffer(gl.ARRAY_BUFFER, imageVertexBuffer);
-	gl.vertexAttribPointer(imageCoordLocation, 2, gl.FLOAT, false, 0, 0);
-	gl.enableVertexAttribArray(imageCoordLocation);
-
-
-	gl.bindBuffer(gl.ARRAY_BUFFER, imageTexCoordBuffer);
-	gl.vertexAttribPointer(imageTexCoordLocation, 2, gl.FLOAT, false, 0, 0);
-	gl.enableVertexAttribArray(imageTexCoordLocation);
-	
-	
-	gl.bindTexture(gl.TEXTURE_2D, texture);
- 
-	// Set the parameters so we can render any size image.
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-
-	
-	gl.drawArrays(gl.TRIANGLES, 0, 6);
-}
-
-
-function getGLContext()
-{
-	var canvas = document.getElementById("canvas-element-id");
-	if (canvas == null)
-	{
-		alert("there is no canvas on this page");
-		return;
-	}
-	var names = ["webgl", "experimental-webgl", "webkit-3d", "moz-webgl"];
-	var ctx = null;
-	for (var i = 0; i < names.length; ++i) 
-	{
-		try 
-		{
-			ctx = canvas.getContext(names[i]);
-		} 
-		catch(e) {}
-		if (ctx) 
-			break;
-	}
-	if (ctx == null)
-	{
-		alert("WebGL is not available");
-	}
-	else
-	{
-		return ctx;
-	}
-}
-
-
-function init()
-{
-	gl = getGLContext();
-	
-	pointShaderProgram = createPointShaderProgram();
-	
-	pointVertexBuffer = gl.createBuffer();
-
-	pointCoordLocation = gl.getAttribLocation(pointShaderProgram, "a_position");
-	pointResolutionUniformLocation = gl.getUniformLocation(pointShaderProgram, "u_resolution");
-	pointOffsetUniformLocation = gl.getUniformLocation(pointShaderProgram, "u_offset");
-	pointScaleUniformLocation = gl.getUniformLocation(pointShaderProgram, "u_scale");
-	
-
-	imageShaderProgram = createImageShaderProgram();
-	
-	imageVertexBuffer = gl.createBuffer();
-	imageTexCoordBuffer = gl.createBuffer();
-
-	imageCoordLocation = gl.getAttribLocation(imageShaderProgram, "a_position");
-	imageTexCoordLocation = gl.getAttribLocation(imageShaderProgram, "a_texCoord");
-	imageResolutionUniformLocation = gl.getUniformLocation(imageShaderProgram, "u_resolution");
-	imageOffsetUniformLocation = gl.getUniformLocation(imageShaderProgram, "u_offset");
-	imageScaleUniformLocation = gl.getUniformLocation(imageShaderProgram, "u_scale");
-	
-	
-	var image = new Image();
-	image.onload = function() {
-		gl.bindBuffer(gl.ARRAY_BUFFER, imageVertexBuffer);
-		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([ 
-			0, 0,
-			image.width, 0,
-			0, image.height,
-			0, image.height,
-			image.width, 0,
-			image.width, image.height]), gl.STATIC_DRAW);
-			
-		gl.bindBuffer(gl.ARRAY_BUFFER, imageTexCoordBuffer);
-		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-			0.0,  0.0,
-			1.0,  0.0,
-			0.0,  1.0,
-			0.0,  1.0,
-			1.0,  0.0,
-			1.0,  1.0]), gl.STATIC_DRAW);
-			
-		texture = gl.createTexture();
-		gl.bindTexture(gl.TEXTURE_2D, texture);
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-	
-		renderFrame();
-	}
-	image.src = "Image.png";
-}
-
-
-function createPointShaderProgram()
-{
-	var vertCode =
-		'attribute vec3 a_position;' +
-		
-		'uniform vec2 u_resolution;' +
-		'uniform vec2 u_offset;' +
-		'uniform float u_scale;' +
-			
-		'void main(void) {' +
-			'vec2 clipSpace = ((vec2(a_position * u_scale) - u_offset) / u_resolution) * 2.0 - 1.0;' + 
-			'gl_Position = vec4(clipSpace * vec2(1, -1), 0.0, 1.0);' +
-			'gl_PointSize = a_position.z;'+
-		'}';
-	 var vertShader = gl.createShader(gl.VERTEX_SHADER);
-	 gl.shaderSource(vertShader, vertCode);
-	 gl.compileShader(vertShader);
-
-	 var fragCode =
-		'void main(void) {' +
-			' gl_FragColor = vec4(0.0, 0.5, 0.0, 1.0);' +
-		'}';
-	 var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
-	 gl.shaderSource(fragShader, fragCode);
-	 gl.compileShader(fragShader);
-
-
-	 var shaderProgram = gl.createProgram();
-	 gl.attachShader(shaderProgram, vertShader); 
-	 gl.attachShader(shaderProgram, fragShader);
-	 gl.linkProgram(shaderProgram);
-	 
-	 return shaderProgram;
-}
-
-
-function createImageShaderProgram()
-{
-	var vertCode =
-		'attribute vec2 a_position;' +
-		'attribute vec2 a_texCoord;' +
-		
-		'uniform vec2 u_resolution;' +
-		'uniform vec2 u_offset;' +
-		'uniform float u_scale;' +
-		
-		'varying vec2 v_texCoord;' +
-			
-		'void main(void) {' +
-			'vec2 clipSpace = ((vec2(a_position * u_scale) - u_offset) / u_resolution) * 2.0 - 1.0;' + 
-			'gl_Position = vec4(clipSpace * vec2(1, -1), 0.0, 1.0);' +
-			'v_texCoord = a_texCoord;' +
-		'}';
-	 var vertShader = gl.createShader(gl.VERTEX_SHADER);
-	 gl.shaderSource(vertShader, vertCode);
-	 gl.compileShader(vertShader);
-
-	 var fragCode =
-		'precision mediump float;' +
- 
-		'uniform sampler2D u_image;' +
-		 
-		'varying vec2 v_texCoord;' +
-		 
-		'void main() {' +
-		   'gl_FragColor = texture2D(u_image, v_texCoord);' +
-		'}';
-	 var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
-	 gl.shaderSource(fragShader, fragCode);
-	 gl.compileShader(fragShader);
-
-
-	 var shaderProgram = gl.createProgram();
-	 gl.attachShader(shaderProgram, vertShader); 
-	 gl.attachShader(shaderProgram, fragShader);
-	 gl.linkProgram(shaderProgram);
-	 
-	 return shaderProgram;
 }
